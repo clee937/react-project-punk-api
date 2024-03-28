8095c8f661f728eff8983f4e093330e3030e0c60
Cheryl Lee <clee0937@gmail.com>
2024-03-28 16:47:40 +0000

    added props object and tsx to BeerCard component

--- src/Components/BeerCard/BeerCard.tsx
+++ src/Components/BeerCard/BeerCard.tsx
@@ -1,7 +1,19 @@
 import "./BeerCard.scss";
 
-const BeerCard = () => {
-  return <div className="beer-card">Beer card</div>;
+type BeerCardProps = {
+  imageUrl: string;
+  name: string;
+  info: string;
+};
+
+const BeerCard = ({ imageUrl, name, info }: BeerCardProps) => {
+  return (
+    <div className="beer-card">
+      <img src={imageUrl} alt={name} />
+      <h2>{name}</h2>
+      <p>{info}</p>
+    </div>
+  );
 };
 
 export default BeerCard;
