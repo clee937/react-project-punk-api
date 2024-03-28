3060423b6058e174c5ffab86872dbcbdf23fa514
Cheryl Lee <clee0937@gmail.com>
2024-03-28 13:08:37 +0000

    created files for Main component

---
+++ src/Components/Main/Main.tsx
@@ -0,0 +1,18 @@
+import BeerCard from "../BeerCard/BeerCard";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <div className="main">
+      Main container
+      <BeerCard />
+      <BeerCard />
+      <BeerCard />
+      <BeerCard />
+      <BeerCard />
+      <BeerCard />
+    </div>
+  );
+};
+
+export default Main;
