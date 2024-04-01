23ea5c790fd75ebbf8a580c35131d6317f2ddd48
Cheryl Lee <clee0937@gmail.com>
2024-04-01 21:28:38 +0100

    added state, event handler and filter logic to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import "./App.scss";
+import { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import beers from "./data/beers";
+import { Beer } from "./types/types";
+import { FormEvent } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    const input = event.currentTarget.value.toLowerCase();
+    setSearchTerm(input);
+  };
+
+  const filteredBeers: Beer[] = beers.filter((beer: Beer) => {
+    return beer.name.toLowerCase().includes(searchTerm);
+  });
+
   return (
     <div className="app">
-      <Nav />
-      <Main />
+      <Nav searchTerm={searchTerm} handleInput={handleInput} />
+      <Main filteredBeers={filteredBeers} />
     </div>
   );
 }
