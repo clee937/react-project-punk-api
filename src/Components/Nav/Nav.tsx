28dc120ee08db2e4a9bb460a945382c3d6379758
Cheryl Lee <clee0937@gmail.com>
2024-04-01 21:26:27 +0100

    added props objectand SearchBar component to Nav

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,7 +1,23 @@
+import SearchBar from "../SearchBar/SearchBar";
+import { FormEventHandler } from "react";
 import "./Nav.scss";
 
-const Nav = () => {
-  return <div className="nav">Nav bar</div>;
+type NavProps = {
+  searchTerm: string;
+  handleInput: FormEventHandler<HTMLInputElement>;
+};
+
+const Nav = ({ searchTerm, handleInput }: NavProps) => {
+  return (
+    <div className="nav">
+      <SearchBar
+        label="Beer"
+        searchTerm={searchTerm}
+        placeHolderText="Search by name..."
+        handleInput={handleInput}
+      />
+    </div>
+  );
 };
 
 export default Nav;
