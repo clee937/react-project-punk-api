1a6c50265b1151be135b032a3c4e4de4d45d735b
Cheryl Lee <clee0937@gmail.com>
2024-04-02 22:00:54 +0100

    added abv to BeerCard

--- src/Components/BeerCard/BeerCard.tsx
+++ src/Components/BeerCard/BeerCard.tsx
@@ -4,16 +4,17 @@ type BeerCardProps = {
   imageUrl: string;
   name: string;
   info: string;
-  key: number;
+  abv: number;
 };
 
-const BeerCard = ({ imageUrl, name, info, key }: BeerCardProps) => {
+const BeerCard = ({ imageUrl, name, info, abv }: BeerCardProps) => {
   return (
-    <div className="beer-card" key={key}>
+    <div className="beer-card">
       <div className="beer-card__image-container">
         <img className="beer-card__image" src={imageUrl} alt={name} />
       </div>
       <h2 className="beer-card__name">{name}</h2>
+      <p className="beer-card__abv">{abv}</p>
       <p className="beer-card__info">{info}</p>
     </div>
   );
