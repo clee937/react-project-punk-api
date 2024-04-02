f4d36d259ad785d0e7e924355ffb2b494af44469
Cheryl Lee <clee0937@gmail.com>
2024-04-02 21:55:15 +0100

    added Checkbox component to Nav.tsx

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,21 +1,47 @@
 import SearchBar from "../SearchBar/SearchBar";
 import { FormEventHandler } from "react";
 import "./Nav.scss";
+import Checkbox from "../Checkbox/Checkbox";
 
 type NavProps = {
   searchTerm: string;
-  handleInput: FormEventHandler<HTMLInputElement>;
+  noOfResults: number;
+  handleSearchbarInput: FormEventHandler<HTMLInputElement>;
+  handleAbvCheckboxInput: () => void;
+  handleClassicRangeCheckboxInput: () => void;
 };
 
-const Nav = ({ searchTerm, handleInput }: NavProps) => {
+const Nav = ({
+  searchTerm,
+  noOfResults,
+  handleSearchbarInput,
+  handleAbvCheckboxInput,
+  handleClassicRangeCheckboxInput,
+}: NavProps) => {
   return (
     <div className="nav">
       <SearchBar
         label="Beer"
         searchTerm={searchTerm}
         placeHolderText="Search by name..."
-        handleInput={handleInput}
+        handleInput={handleSearchbarInput}
       />
+      <Checkbox
+        label="ABV greater than 6%"
+        id="abv"
+        name="abv"
+        value="abv"
+        handleAbvCheckboxInput={handleAbvCheckboxInput}
+      />
+
+      <Checkbox
+        label="Classic Range"
+        id="classic"
+        name="classic"
+        value="classic"
+        handleAbvCheckboxInput={handleClassicRangeCheckboxInput}
+      />
+      <p>Number of results: {noOfResults}</p>
     </div>
   );
 };
